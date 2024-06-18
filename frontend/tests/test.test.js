ecd5da3694ef4c3ec1b35316811440b2c3b8df05
devinatirza <devinatirza8@gmail.com>
2024-06-18 11:57:14 +0700

    JavaScript test deleted

--- frontend/tests/test.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react-native';
-import LoginScreen from '../pages/Login';
-
-test('renders login screen correctly', () => {
-  const { getByText } = render(<LoginScreen />);
-  expect(getByText('Login')).toBeTruthy();
-});
